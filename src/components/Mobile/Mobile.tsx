62da099405e3d3afac03c12c9b6faa977958e0e6
Hanan Asyrawi <hasyrawi@gmail.com>
2020-11-21 05:39:37 +0800

    update padding

--- src/components/Mobile/Mobile.tsx
+++ src/components/Mobile/Mobile.tsx
@@ -9,24 +9,24 @@ interface Props {
 
 const Mobile = (props: Props) => {
     return (
-        <div className="fixed md:hidden bottom-0 bg-gray-800 h-16 min-w-full border-white border-t-2">
+        <div className="fixed md:hidden bottom-0 bg-gray-800 h-20 min-w-full border-white border-t-2">
             <div className="mobile_nav_bar">
-                <div className="item flex justify-around mx-3 pt-1">
+                <div className="item flex justify-around pt-3">
                     <NextLink href="/">
                         <a>
                             <IconHome className="text-gray-300 text-4xl antialiased cursor-pointer hover:text-yellow-300" />
-                            <span className="text-gray-200 hover:text-yellow-300">Home</span>
+                            <span className="text-gray-200 hover:text-yellow-300 mb-1">Home</span>
                         </a>
                     </NextLink>
                     <NextLink href="/blog">
                         <a>
-                            <IconCode className="text-gray-300 text-4xl antialiased cursor-pointer hover:text-yellow-300" />
+                            <IconCode className="text-gray-300 text-4xl text-center items-center antialiased cursor-pointer hover:text-yellow-300" />
                             <span className="text-gray-200 hover:text-yellow-300">Blog</span>
                         </a>
                     </NextLink>
                     <NextLink href="/about">
                         <a>
-                            <IconAt className="text-gray-300 text-4xl antialiased cursor-pointer hover:text-yellow-300" />
+                            <IconAt className="text-gray-300 text-center text-4xl antialiased cursor-pointer hover:text-yellow-300" />
                             <span className="text-gray-200 hover:text-yellow-300 text-center items-center">About</span>
                         </a>
                     </NextLink>
