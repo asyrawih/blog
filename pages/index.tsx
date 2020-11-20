ea7944ddc60445f5aa284d8a58b7d8de0ae0ffcb
Hanan Asyrawi <hasyrawi@gmail.com>
2020-11-20 19:42:46 +0800

    init next

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="bg-gray-700">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="mx-auto">
+        <p>Helllo</p>
+      </div>
+    </div>
+  )
+}
