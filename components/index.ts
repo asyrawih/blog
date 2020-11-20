e96f63814bdcd3f3b41e7bed307663bf9a768d1c
Hanan Asyrawi <hasyrawi@gmail.com>
2020-11-20 23:36:41 +0800

    Layouting

---
+++ components/index.ts
@@ -0,0 +1,13 @@
+import Container from './Container/Container'
+import Card from './Card/Card'
+import CardImage from './Card/CardImage'
+import Tags from './Tags/Tags'
+import Tag from './Tags/Tag'
+
+export {
+    Container,
+    Card,
+    CardImage,
+    Tags,
+    Tag
+}
